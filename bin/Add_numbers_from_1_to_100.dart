fef49c23120b13f7feefd88aa9da853bec4954ac
ao307 <56132081+ao307@users.noreply.github.com>
2021-03-25 13:21:58 +0200

    Add the numbers from 1 to 100

---
+++ bin/Add_numbers_from_1_to_100.dart
@@ -0,0 +1,5 @@
+import 'package:Add_numbers_from_1_to_100/Add_numbers_from_1_to_100.dart' as Add_numbers_from_1_to_100;
+
+void main(List<String> arguments) {
+  print('Hello world: ${Add_numbers_from_1_to_100.calculate()}!');
+}
---
+++ test/Add_numbers_from_1_to_100_test.dart
@@ -0,0 +1,7 @@
+void main() {
+  int Addtion = 0;
+  for (int i = 0; i <= 100; i++) {
+    Addtion += i;
+  }
+  print(Addtion);
+}
